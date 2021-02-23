b9b6f748700ceeefbbfe7fe63b1eb181d8e3364f
Amilkar Munoz <71573508+soloamilkar@users.noreply.github.com>
2021-02-22 21:44:57 -0600

    Script Uploaded

---
+++ wiki.py
@@ -0,0 +1,30 @@
+import time
+import re
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+DRIVER_PATH = "" # insert here your chromedriver path 
+
+driver = webdriver.Chrome(DRIVER_PATH)
+driver.get("https://wikipedia.org")
+
+try:
+    searchbox = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'searchInput'))
+    )
+    searchbox.clear()
+    searchbox.send_keys('samurai', Keys.ENTER)
+    content =  WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.TAG_NAME, 'p'))
+    )
+    count = 1
+    with open('Samurais.txt', 'w', encoding="utf8") as file:
+        count += 1
+        file.write("\n".join([p.text for p in content]))
+
+
+finally:
+    driver.close()
